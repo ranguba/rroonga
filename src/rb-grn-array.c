71a255ba862d6c260045d45f76536ee35de70c6b
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-04-28 07:29:10 +0000

    * document. * improve Groonga::Array.create API.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@209 2d713de4-4fbb-4d10-8406-bb89ef438322

--- src/rb-grn-array.c
+++ src/rb-grn-array.c
@@ -22,12 +22,122 @@
 
 VALUE rb_cGrnArray;
 
+/*
+ * Document-class: Groonga::Array < Groonga::Table
+ *
+ * 各レコードにキーが存在しないテーブル。レコードはIDで識別
+ * する。
+ */
+
+/*
+ * call-seq:
+ *   Groonga::Array.create(options={})            -> Groonga::Array
+ *   Groonga::Array.create(options={}) {|table| ... }
+ *
+ * キーのないテーブルを生成する。ブロックを指定すると、そのブ
+ * ロックに生成したテーブルが渡され、ブロックを抜けると自動的
+ * にテーブルが破棄される。
+ *
+ * _options_に指定可能な値は以下の通り。
+ *
+ * [+:context+]
+ *   テーブルが利用するGroonga::Context。省略すると
+ *   Groonga::Context.defaultを用いる。
+ *
+ * [+:name+]
+ *   テーブルの名前。名前をつけると、Groonga::Context#[]に名
+ *   前を指定してテーブルを取得することができる。省略すると
+ *   無名テーブルになり、テーブルIDでのみ取得できる。
+ *
+ * [+:path+]
+ *   テーブルを保存するパス。パスを指定すると永続テーブルとな
+ *   り、プロセス終了後もレコードは保持され、次回起動時に
+ *   Groonga::Array.openで保存されたレコードを利用することが
+ *   できる。省略すると一時テーブルになり、プロセスが終了する
+ *   とレコードは破棄される。
+ *
+ * [+:persistent+]
+ *   +true+を指定すると永続テーブルとなる。+path+を省略した
+ *   場合は自動的にパスが付加される。+:context+で指定した
+ *   Groonga::Contextに結びついているデータベースが一時デー
+ *   タベースの場合は例外が発生する。
+ *
+ * [+:value_size+]
+ *   値の大きさを指定する。省略すると0になる。
+ *
+ * 無名一時テーブルを生成する。
+ *   Groonga::Array.create
+ *
+ * 無名永続テーブルを生成する。
+ *   Groonga::Array.create(:path => "/tmp/array.grn")
+ *
+ * 名前付き永続テーブルを生成する。ただし、ファイル名は気に
+ * しない。
+ *   Groonga::Array.create(:name => "<bookmarks>",
+ *                         :persistent => true)
+ *
+ * それぞれのレコードに512バイトの値を格納できる無名一時テー
+ * ブルを生成する。
+ *   Groonga::Array.create(:value => 512)
+ */
 static VALUE
-rb_grn_array_s_create (int argc, VALUE *argv, VALUE self)
+rb_grn_array_s_create (int argc, VALUE *argv, VALUE klass)
 {
-    return rb_grn_table_s_create(argc, argv, self, GRN_TABLE_NO_KEY);
+    grn_ctx *context;
+    grn_obj *table;
+    const char *name = NULL, *path = NULL;
+    unsigned name_size = 0, value_size = 0;
+    grn_obj_flags flags = GRN_TABLE_NO_KEY;
+    VALUE rb_table;
+    VALUE options, rb_context, rb_name, rb_path, rb_persistent;
+    VALUE rb_value_size;
+
+    rb_scan_args(argc, argv, "01", &options);
+
+    rb_grn_scan_options(options,
+			"context", &rb_context,
+			"name", &rb_name,
+                        "path", &rb_path,
+			"persistent", &rb_persistent,
+			"value_size", &rb_value_size,
+			NULL);
+
+    context = rb_grn_context_ensure(rb_context);
+
+    if (!NIL_P(rb_name)) {
+        name = StringValuePtr(rb_name);
+	name_size = RSTRING_LEN(rb_name);
+    }
+
+    if (!NIL_P(rb_path)) {
+        path = StringValueCStr(rb_path);
+	flags |= GRN_OBJ_PERSISTENT;
+    }
+
+    if (RVAL2CBOOL(rb_persistent))
+	flags |= GRN_OBJ_PERSISTENT;
+
+    if (!NIL_P(rb_value_size))
+	value_size = NUM2UINT(rb_value_size);
+
+    table = grn_table_create(context, name, name_size, path,
+			     flags, NULL, value_size,
+			     GRN_ENC_NONE);
+    rb_table = GRNOBJECT2RVAL(klass, context, table);
+    rb_grn_context_check(context, rb_table);
+
+    if (rb_block_given_p())
+        return rb_ensure(rb_yield, rb_table, rb_grn_object_close, rb_table);
+    else
+        return rb_table;
 }
 
+/*
+ * call-seq:
+ *  array.add -> Groonga::Record
+ *
+ * 新しくレコードを生成します。
+ */
 static VALUE
 rb_grn_array_add (VALUE self)
 {
