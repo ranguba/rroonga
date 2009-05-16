3267ebc68446b1d99f7500345d87f6b2af95043b
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-05-16 15:08:28 +0000

    * cleanup.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@324 2d713de4-4fbb-4d10-8406-bb89ef438322

---
+++ benchmark/common.rb
@@ -0,0 +1,49 @@
+require 'benchmark'
+require 'tempfile'
+
+def memory_usage()
+  status = `cat /proc/#{$$}/status`
+  lines = status.split("\n")
+  lines.each do |line|
+    if line =~ /^VmRSS:/
+      line.gsub!(/.*:\s*(\d+).*/, '\1')
+      return line.to_i / 1024.0
+    end
+  end
+  return -1;
+end
+
+@items = []
+
+def item(label, &block)
+  @items << [label, block]
+end
+
+def report(index=0)
+  width = @items.collect do |label, _|
+    label.length
+  end.max
+
+  label, block = @items[index]
+  if label.nil?
+    puts "unavailable report ID: #{index}"
+    puts "available IDs:"
+    @items.each_with_index do |(label, block), i|
+      puts "#{i}: #{label}"
+    end
+    exit 1
+  end
+
+  if index.zero?
+    puts(" " * (width - 1) + Benchmark::Tms::CAPTION.rstrip + "memory".rjust(14))
+  end
+  # GC.disable
+  before = memory_usage
+  result = Benchmark.measure(&block)
+  # GC.enable
+  GC.start
+  size = memory_usage - before
+
+  formatted_size = "%10.3f" % size
+  puts "#{label.ljust(width)} #{result.to_s.strip} #{formatted_size}MB"
+end
