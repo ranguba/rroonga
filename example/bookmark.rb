312af0a9708dc4c3b81d64147044697a94a6df51
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-05-31 15:23:30 +0000

    * update tutorial.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@358 2d713de4-4fbb-4d10-8406-bb89ef438322

--- example/bookmark.rb
+++ example/bookmark.rb
@@ -10,28 +10,77 @@ Groonga::Context.default_options = {:encoding => :utf8}
 path = ARGV[0]
 persistent = !path.nil?
 
-Groonga::Database.create(:path => path)
+p Groonga::Database.create(:path => path)
 
-items = Groonga::Hash.create(:name => "<items>", :persistent => persistent)
+p(items = Groonga::Hash.create(:name => "<items>", :persistent => persistent))
 
-items.add("http://ja.wikipedia.org/wiki/Ruby")
-items.add("http://www.ruby-lang.org/")
+p items.add("http://ja.wikipedia.org/wiki/Ruby")
+p items.add("http://www.ruby-lang.org/")
 
-title_column = items.define_column("title", "<text>",
-                                   :persistent => persistent)
+p items.define_column("title", "<text>", :persistent => persistent)
 
-terms = Groonga::Hash.create(:name => "<terms>",
-                             :key_type => "<shorttext>",
-                             :persistent => persistent)
-terms.default_tokenizer = "<token:bigram>"
-title_index_column = terms.define_index_column("item_title", items,
-                                               :with_position => true,
-                                               :persistent => persistent)
-title_index_column.source = title_column
+p(terms = Groonga::Hash.create(:name => "<terms>",
+                               :key_type => "<shorttext>",
+                               :persistent => persistent,
+                               :default_tokenizer => "<token:bigram>"))
+p terms.define_index_column("item_title", items,
+                            :persistent => persistent,
+                            :source => "<items>.title")
 
-items.find("http://ja.wikipedia.org/wiki/Ruby")["title"] = "Ruby"
-items.find("http://www.ruby-lang.org/")["title"] = "オブジェクトスクリプト言語Ruby"
+p items.find("http://ja.wikipedia.org/wiki/Ruby")["title"] = "Ruby"
+p items.find("http://www.ruby-lang.org/")["title"] = "オブジェクトスクリプト言語Ruby"
 
-p terms.collect(&:key)
-p title_index_column.search("Ruby").size
-p title_index_column.search("ruby").size
+p(users = Groonga::Hash.create(:name => "<users>",
+                               :key_type => "<shorttext>",
+                               :persistent => persistent))
+p users.define_column("name", "<text>",
+                      :persistent => persistent)
+
+p(comments = Groonga::Array.create(:name => "<comments>",
+                                   :persistent => persistent))
+p comments.define_column("item", items)
+p comments.define_column("author", users)
+p comments.define_column("content", "<text>")
+p comments.define_column("issued", "<time>")
+
+p terms.define_index_column("comment_content", comments,
+                            :persistent => persistent,
+                            :source => "<comments>.content")
+
+p users.add("moritan", :name => "モリタン")
+p users.add("taporobo", :name => "タポロボ")
+
+
+p items.find("http://d.hatena.ne.jp/brazil/20050829/1125321936")
+
+p items.add("http://d.hatena.ne.jp/brazil/20050829/1125321936",
+            :title => "[翻訳]JavaScript: 世界で最も誤解されたプログラミング言語")
+
+p comments.add(:item => "http://d.hatena.ne.jp/brazil/20050829/1125321936",
+               :author => "moritan",
+               :content => "JavaScript LISP",
+               :issued => 1187430026)
+
+@items = items
+@comments = comments
+def add_bookmark(url, title, author, content, issued)
+  item = @items.find(url) || @items.add(url, :title => title)
+  @comments.add(:item => item,
+                :author => author,
+                :content => content,
+                :issued => issued)
+end
+
+p add_bookmark("http://practical-scheme.net/docs/cont-j.html",
+               "なんでも継続", "moritan", "継続 LISP Scheme", 1187568692)
+p add_bookmark("http://d.hatena.ne.jp/higepon/20070815/1187192864",
+               "末尾再帰", "taporobo", "末尾再帰 Scheme LISP", 1187568793)
+p add_bookmark("http://practical-scheme.net/docs/cont-j.html",
+               "なんでも継続", "taporobo", "トランポリン LISP continuation",
+               1187568692)
+
+terms.column("comment_content").search("LISP").each do |record|
+  record = record.key
+  p [record[".issued"], record[".item.title"], record[".author.name"],
+     record[".content"]]
+end
