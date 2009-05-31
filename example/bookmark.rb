04c1ec41b7b52dcd7c2e51e13c56c56ee2a5eff3
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-05-31 10:59:55 +0000

    * follow the recent changes.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@344 2d713de4-4fbb-4d10-8406-bb89ef438322

--- example/bookmark.rb
+++ example/bookmark.rb
@@ -22,16 +22,15 @@ title_column = items.define_column("title", "<text>",
 
 terms = Groonga::Hash.create(:name => "<terms>",
                              :key_type => "<shorttext>",
-                             :key_normalize => true,
                              :persistent => persistent)
-title_index_column = terms.define_column("item_title", items,
-                                         :type => "index",
-                                         :with_position => true,
-                                         :persistent => persistent)
+terms.default_tokenizer = "<token:bigram>"
+title_index_column = terms.define_index_column("item_title", items,
+                                               :with_position => true,
+                                               :persistent => persistent)
 title_index_column.source = title_column
 
-items["http://ja.wikipedia.org/wiki/Ruby"]["title"] = "Ruby"
-items["http://www.ruby-lang.org/"]["title"] = "オブジェクトスクリプト言語Ruby"
+items.find("http://ja.wikipedia.org/wiki/Ruby")["title"] = "Ruby"
+items.find("http://www.ruby-lang.org/")["title"] = "オブジェクトスクリプト言語Ruby"
 
 p terms.collect(&:key)
 p title_index_column.search("Ruby").size
