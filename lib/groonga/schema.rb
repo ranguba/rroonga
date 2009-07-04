1eac495f4c1c9121751a5fdd1486ebc28b21233f
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-07-04 11:11:46 +0000

    * support column definition.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@424 2d713de4-4fbb-4d10-8406-bb89ef438322

--- lib/groonga/schema.rb
+++ lib/groonga/schema.rb
@@ -30,13 +30,73 @@ module Groonga
     class TableDefinition
       def initialize(name, options)
         @name = name
+        @name = @name.to_s if @name.is_a?(Symbol)
+        @columns = []
         @options = options
         @table_type = table_type
       end
 
       def create
         table = @table_type.create(:name => @name, :path => @options[:path])
+        @columns.each do |column|
+          column.create(table)
+        end
         table
+      end
+
+      def column(name, type, options={})
+        column = self[name] || ColumnDefinition.new(name, options)
+        column.type = type
+        column.options.merge!(options)
+        @columns << column unless @columns.include?(column)
+        self
+      end
+
+      def integer32(name, options={})
+        column(name, "<int>", options)
+      end
+      alias_method :integer, :integer32
+      alias_method :int32, :integer32
+
+      def integer64(name, options={})
+        column(name, "<int64>", options)
+      end
+      alias_method :int64, :integer64
+
+      def unsigned_integer32(name, options={})
+        column(name, "<uint>", options)
+      end
+      alias_method :unsigned_integer, :unsigned_integer32
+      alias_method :uint32, :unsigned_integer32
+
+      def unsigned_integer64(name, options={})
+        column(name, "<uint64>", options)
+      end
+      alias_method :uint64, :unsigned_integer64
+
+      def float(name, options={})
+        column(name, "<float>", options)
+      end
+
+      def time(name, options={})
+        column(name, "<time>", options)
+      end
+
+      def short_text(name, options={})
+        column(name, "<shorttext>", options)
+      end
+      alias_method :string, :short_text
+
+      def text(name, options={})
+        column(name, "<text>", options)
+      end
+
+      def long_text(name, options={})
+        column(name, "<longtext>", options)
+      end
+
+      def [](name)
+        @columns.find {|column| column.name == name}
       end
 
       private
@@ -51,6 +111,52 @@ module Groonga
           Groonga::PatriciaTrie
         else
           raise ArgumentError, "unknown table type: #{type.inspect}"
+        end
+      end
+
+      def context
+        @options[:context] || Groonga::Context.default
+      end
+    end
+
+    class ColumnDefinition
+      attr_accessor :name, :type
+      attr_reader :options
+
+      def initialize(name, options={})
+        @name = name
+        @name = @name.to_s if @name.is_a?(Symbol)
+        @options = (options || {}).dup
+        @type = nil
+      end
+
+      def create(table)
+        table.define_column(@name,
+                            normalize_type(@type),
+                            @options)
+      end
+
+      def normalize_type(type)
+        return type if type.is_a?(Groonga::Object)
+        case type.to_s
+        when "string"
+          "<shorttext>"
+        when "text"
+          "<text>"
+        when "integer"
+          "<int>"
+        when "float"
+          "<float>"
+        when "decimal"
+          "<int64>"
+        when "datetime", "timestamp", "time", "date"
+          "<time>"
+        when "binary"
+          "<longtext>"
+        when "boolean"
+          "<int>"
+        else
+          type
         end
       end
     end
--- test/test-schema.rb
+++ test/test-schema.rb
@@ -24,4 +24,76 @@ class SchemaTest < Test::Unit::TestCase
     end
     assert_not_nil(context["<posts>"])
   end
+
+  def test_integer32_column
+    assert_nil(context["<posts>.rate"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.integer32 :rate
+    end
+    assert_equal(context["<int>"], context["<posts>.rate"].range)
+  end
+
+  def test_integer64_column
+    assert_nil(context["<posts>.rate"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.integer64 :rate
+    end
+    assert_equal(context["<int64>"], context["<posts>.rate"].range)
+  end
+
+  def test_unsigned_integer32_column
+    assert_nil(context["<posts>.n_viewed"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.unsigned_integer32 :n_viewed
+    end
+    assert_equal(context["<uint>"], context["<posts>.n_viewed"].range)
+  end
+
+  def test_unsigned_integer64_column
+    assert_nil(context["<posts>.n_viewed"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.unsigned_integer64 :n_viewed
+    end
+    assert_equal(context["<uint64>"], context["<posts>.n_viewed"].range)
+  end
+
+  def test_float_column
+    assert_nil(context["<posts>.rate"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.float :rate
+    end
+    assert_equal(context["<float>"], context["<posts>.rate"].range)
+  end
+
+  def test_time_column
+    assert_nil(context["<posts>.last_modified"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.time :last_modified
+    end
+    assert_equal(context["<time>"], context["<posts>.last_modified"].range)
+  end
+
+  def test_short_text_column
+    assert_nil(context["<posts>.title"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.short_text :title
+    end
+    assert_equal(context["<shorttext>"], context["<posts>.title"].range)
+  end
+
+  def test_text_column
+    assert_nil(context["<posts>.comment"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.text :comment
+    end
+    assert_equal(context["<text>"], context["<posts>.comment"].range)
+  end
+
+  def test_long_text_column
+    assert_nil(context["<posts>.content"])
+    Groonga::Schema.create_table("<posts>") do |table|
+      table.long_text :content
+    end
+    assert_equal(context["<longtext>"], context["<posts>.content"].range)
+  end
 end
