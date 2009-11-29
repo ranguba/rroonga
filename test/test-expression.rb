74d95d46a6188f51041c1c8f2609cc5db6425c7e
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-11-29 08:21:30 +0000

    * PLUS is supported in groonga.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@822 2d713de4-4fbb-4d10-8406-bb89ef438322

--- test/test-expression.rb
+++ test/test-expression.rb
@@ -63,7 +63,7 @@ class ExpressionTest < Test::Unit::TestCase
     expression.append_operation(Groonga::Operation::PLUS, 2)
     expression.compile
 
-    assert_equal("#<Groonga::Expression noname(){1 1}>", expression.inspect)
+    assert_equal("#<Groonga::Expression noname(){1 1 PLUS}>", expression.inspect)
   end
 
   def test_snippet
