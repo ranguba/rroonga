1a8ddf5dd94366e8b0c69cbf3c2e111c833860e1
ryoqun <ryoqun@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-12-21 05:32:15 +0000

    add document of Groonga::Logger
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/groonga/trunk@897 2d713de4-4fbb-4d10-8406-bb89ef438322

--- ext/rb-grn-logger.c
+++ ext/rb-grn-logger.c
@@ -18,6 +18,13 @@
 
 #include "rb-grn.h"
 
+/*
+ * Document-class: Groonga::Logger
+ *
+ * groongaから出力されるログを記録するためのクラス。
+ *
+ */
+
 #define RVAL2GRNWRAPPER(object)  (rb_grn_logger_info_wrapper_from_ruby_object(object))
 #define RVAL2GRNLOGLEVEL(object) (rb_grn_log_level_from_ruby_object(object))
 #define GRNLOGLEVEL2RVAL(level)  (rb_grn_log_level_to_ruby_object(level))
@@ -235,6 +242,47 @@ rb_grn_logger_initialize (int argc, VALUE *argv, VALUE self)
     return Qnil;
 }
 
+/*
+ * call-seq:
+ *   Groonga::Logger.register(options={})
+       {|level, time, title, message, location| ...}
+ *
+ * groongaがログを出力する度に呼び出されるブロックを登録す
+ * る。
+ *
+ * ブロックに渡されてくる引数は_level_, _time_, _title_,
+ * _message_, _location_の5つで、_level_はSymbol、それ以外は
+ * 全て文字列で渡される。その4つについては_options_で+false+
+ * を指定することでブロックに渡さないようにすることができ、
+ * その場合は空文字列が実際には渡される。
+ *
+ * _options_に指定可能な値は以下の通り。
+ *
+ * [+:level+]
+ *   ログのレベルを+:none+, +:emergency+, +:alert+,
+ *   +:critical+, +:error+, +:warning+, +:notice+, +:info+,
+ *   +:debug+, +:dump+のいずれかで指定する。それより重要度が
+ *   低いログはブロックに渡されなくなる。デフォルトでは
+ *   +:notice+。
+ *
+ * [+:time+]
+ *   ログが出力された時間をブロックに渡したいなら+true+を指
+ *   定する。デフォルトでは渡す。
+ *
+ * [+:title+]
+ *   ログのタイトルをブロックに渡したいなら+true+を指定す
+ *   る。デフォルトでは渡す。(FIXME: groongaで実装されてい
+ *   ない?)
+ *
+ * [+:message+]
+ *   ログのメッセージをブロックに渡したいなら+true+を指定す
+ *   る。デフォルトでは渡す。
+ *
+ * [+:location+]
+ *   ログの発生元のプロセスIDとgroongaのソースコードのファイ
+ *   ル名、行番号、関数名をブロックに渡したいなら+true+を指
+ *   定する。デフォルトでは渡す。
+ */
 static VALUE
 rb_grn_logger_s_register (int argc, VALUE *argv, VALUE klass)
 {
