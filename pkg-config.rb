3a028a591df3f55c48fd819bf3781d279b41c898
Kouhei Sutou <kou@clear-code.com>
2010-04-22 21:42:39 +0900

    add nil check.

--- pkg-config.rb
+++ pkg-config.rb
@@ -220,7 +220,7 @@ class PackageConfig
       extern "const char *dln_find_exe(const char *, const char *)"
     end
     path = dln.dln_find_exe(pkg_config.to_s, nil)
-    if path.size.zero?
+    if path and path.size.zero?
       nil
     else
       Pathname(path.to_s)
