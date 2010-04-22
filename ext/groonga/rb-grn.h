fed0b1d21b70cd876b8ae2a0725d88ad14110c8b
Kouhei Sutou <kou@clear-code.com>
2010-04-22 23:02:01 +0900

    0.9.4 -> 0.9.5.

--- ext/groonga/rb-grn.h
+++ ext/groonga/rb-grn.h
@@ -69,7 +69,7 @@ RB_GRN_BEGIN_DECLS
 
 #define RB_GRN_MAJOR_VERSION 0
 #define RB_GRN_MINOR_VERSION 9
-#define RB_GRN_MICRO_VERSION 4
+#define RB_GRN_MICRO_VERSION 5
 
 typedef int rb_grn_boolean;
 #define RB_GRN_FALSE (0)
