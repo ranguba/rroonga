e67cd229cc1887400e939e81165de0dc034793ed
Kouhei Sutou <kou@clear-code.com>
2010-08-27 00:41:15 +0900

    GRN_OBJ_WITH_BUFFER -> GRN_OBJ_RING_BUFFER.

--- ext/groonga/rb-grn-object.c
+++ ext/groonga/rb-grn-object.c
@@ -766,8 +766,8 @@ rb_grn_object_inspect_content_flags_with_label (VALUE inspected,
 	rb_ary_push(inspected_flags, rb_str_new2("WITH_WEIGHT"));
     if (flags & GRN_OBJ_WITH_POSITION)
 	rb_ary_push(inspected_flags, rb_str_new2("WITH_POSITION"));
-    if (flags & GRN_OBJ_WITH_BUFFER)
-	rb_ary_push(inspected_flags, rb_str_new2("WITH_BUFFER"));
+    if (flags & GRN_OBJ_RING_BUFFER)
+	rb_ary_push(inspected_flags, rb_str_new2("RING_BUFFER"));
 
     if (flags & GRN_OBJ_WITH_SUBREC) {
 	rb_ary_push(inspected_flags, rb_str_new2("WITH_SUBREC"));
