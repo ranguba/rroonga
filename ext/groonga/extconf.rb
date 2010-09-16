22d40d5c8280236b681dcf868fc67de8e89f627d
Kouhei Sutou <kou@clear-code.com>
2010-09-16 23:45:30 +0900

    support cross compile for Windows.

--- ext/groonga/extconf.rb
+++ ext/groonga/extconf.rb
@@ -37,20 +37,6 @@ package_name = "groonga"
 module_name = "groonga"
 major, minor, micro = RequiredGroongaVersion::VERSION
 
-unless PKGConfig.have_package(package_name, major, minor, micro)
-  have_local_groonga?(package_name, major, minor, micro) or exit 1
-  add_rpath_for_local_groonga
-end
-
-real_version = PKGConfig.modversion(package_name)
-real_major, real_minor, real_micro = real_version.split(/\./)
-
-$defs << "-DRB_GRN_COMPILATION"
-
-$defs << "-DGRN_MAJOR_VERSION=#{real_major}"
-$defs << "-DGRN_MINOR_VERSION=#{real_minor}"
-$defs << "-DGRN_MICRO_VERSION=#{real_micro}"
-
 checking_for(checking_message("GCC")) do
   if macro_defined?("__GNUC__", "")
     $CFLAGS += ' -Wall'
@@ -68,13 +54,26 @@ checking_for(checking_message("Win32 OS")) do
     $DLDFLAGS << " -Wl,--out-implib=#{import_library_name}"
     $cleanfiles << import_library_name
     binary_base_dir = base_dir + "vendor" + "local"
-    $CFLAGS += " -I#{binary_base_dir}/include"
     pkg_config_dir = binary_base_dir + "lib" + "pkgconfig"
     PKGConfig.add_path(pkg_config_dir.to_s)
     PKGConfig.set_override_variable("prefix", binary_base_dir.to_s)
   end
   win32
 end
+
+unless PKGConfig.have_package(package_name, major, minor, micro)
+  have_local_groonga?(package_name, major, minor, micro) or exit 1
+  add_rpath_for_local_groonga
+end
+
+real_version = PKGConfig.modversion(package_name)
+real_major, real_minor, real_micro = real_version.split(/\./)
+
+$defs << "-DRB_GRN_COMPILATION"
+
+$defs << "-DGRN_MAJOR_VERSION=#{real_major}"
+$defs << "-DGRN_MINOR_VERSION=#{real_minor}"
+$defs << "-DGRN_MICRO_VERSION=#{real_micro}"
 
 have_header("ruby/st.h") unless have_macro("HAVE_RUBY_ST_H", "ruby.h")
 have_func("rb_errinfo", "ruby.h")
