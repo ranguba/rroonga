831b932ecce9519311fa613093292c094cc44646
Kouhei Sutou <kou@clear-code.com>
2010-11-25 16:57:06 +0900

    update tutorial.

--- example/bookmark.rb
+++ example/bookmark.rb
@@ -27,7 +27,7 @@ path = ARGV[0]
 if path.nil?
   require 'tmpdir'
   require 'fileutils'
-  temporary_directory = File.join(Dir.tmpdir, "ruby-groonga")
+  temporary_directory = File.join(Dir.tmpdir, "rroonga")
   FileUtils.mkdir_p(temporary_directory)
   at_exit {FileUtils.rm_rf(temporary_directory)}
   path = File.join(temporary_directory, "db")
@@ -36,126 +36,128 @@ persistent = true
 
 p Groonga::Database.create(:path => path)
 
-p(items = Groonga::Hash.create(:name => "Items",
-                               :key_type => "ShortText",
-                               :persistent => persistent))
+p Groonga::Schema.create_table("Items", :type => :hash)
+p(items = Groonga["Items"])
+
+p items.size
 
 p items.add("http://ja.wikipedia.org/wiki/Ruby")
-p items.add("http://www.ruby-lang.org/")
+p items.add("http://www.ruby-lang.org/ja/")
 
-p items.define_column("title", "Text", :persistent => persistent)
+p items.size
 
-p(terms = Groonga::Hash.create(:name => "Terms",
-                               :key_type => "ShortText",
-                               :persistent => persistent,
+p items["http://ja.wikipedia.org/wiki/Ruby"]
+
+p(Groonga::Schema.change_table("Items") do |table|
+    table.text("title")
+  end)
+p(title_column = Groonga["Items.title"])
+
+p(Groonga::Schema.create_table("Terms",
+                               :type => :patricia_trie,
                                :default_tokenizer => "TokenBigram"))
-p terms.define_index_column("item_title", items,
-                            :persistent => persistent,
-                            :with_weight => true,
-                            :with_section => true,
-                            :with_position => true,
-                            :source => "Items.title")
+p(Groonga::Schema.change_table("Terms") do |table|
+    table.index("Items.title")
+  end)
 
-p items.find("http://ja.wikipedia.org/wiki/Ruby")["title"] = "Ruby"
-p items.find("http://www.ruby-lang.org/")["title"] = "オブジェクト指向スクリプト言語Ruby"
+p(items["http://ja.wikipedia.org/wiki/Ruby"].title = "Ruby")
+p(items["http://www.ruby-lang.org/ja/"].title = "オブジェクトスクリプト言語Ruby")
 
-p(users = Groonga::Hash.create(:name => "Users",
-                               :key_type => "ShortText",
-                               :persistent => persistent))
-p users.define_column("name", "Text",
-                      :persistent => persistent)
+p(ruby_items = items.select {|record| record.title =~ "Ruby"})
+p(ruby_items.collect {|record| record.key.key})
+p(ruby_items.collect {|record| record["_key"]})
 
-p(comments = Groonga::Array.create(:name => "Comments",
-                                   :persistent => persistent))
-p comments.define_column("item", items)
-p comments.define_column("author", users)
-p comments.define_column("content", "Text")
-p comments.define_column("issued", "Time")
+p(Groonga::Schema.create_table("Users", :type => :hash) do |table|
+    table.text("name")
+  end)
+p(Groonga::Schema.create_table("Comments") do |table|
+    table.reference("item")
+    table.reference("author", "Users")
+    table.text("content")
+    table.time("issued")
+  end)
 
-p terms.define_index_column("comment_content", comments,
-                            :persistent => persistent,
-                            :with_weight => true,
-                            :with_section => true,
-                            :with_position => true,
-                            :source => "Comments.content")
+p(Groonga::Schema.change_table("Terms") do |table|
+    table.index("Comments.content")
+  end)
 
-p users.add("moritan", :name => "モリタン")
-p users.add("taporobo", :name => "タポロボ")
+p(users = Groonga["Users"])
+p(users.add("moritan", :name => "モリタン"))
+p(users.add("taporobo", :name => "タポロボ"))
 
+p(items.has_key?("http://www.rubyist.net/~matz/"))
+p(items.add("http://www.rubyist.net/~matz/",
+            :title => "Matzにっき"))
 
-p items.find("http://d.hatena.ne.jp/brazil/20050829/1125321936")
+require "time"
 
-p items.add("http://d.hatena.ne.jp/brazil/20050829/1125321936",
-            :title => "[翻訳]JavaScript: 世界で最も誤解されたプログラミング言語")
-
-p comments.add(:item => "http://d.hatena.ne.jp/brazil/20050829/1125321936",
+p(comments = Groonga["Comments"])
+p(comments.add(:item => "http://www.rubyist.net/~matz/",
                :author => "moritan",
-               :content => "JavaScript LISP",
-               :issued => 1187430026)
+               :content => "Ruby Matz",
+               :issued => Time.parse("2010-11-20T18:01:22+09:00")))
 
-@items = items
-@comments = comments
+p(@items = items)
+p(@comments = comments)
 def add_bookmark(url, title, author, content, issued)
-  item = @items.find(url) || @items.add(url, :title => title)
+  item = @items[url] || @items.add(url, :title => title)
   @comments.add(:item => item,
                 :author => author,
                 :content => content,
                 :issued => issued)
 end
 
-p add_bookmark("http://practical-scheme.net/docs/cont-j.html",
-               "なんでも継続", "moritan", "継続 LISP Scheme", 1187568692)
-p add_bookmark("http://d.hatena.ne.jp/higepon/20070815/1187192864",
-               "末尾再帰", "taporobo", "末尾再帰 Scheme LISP", 1187568793)
-p add_bookmark("http://practical-scheme.net/docs/cont-j.html",
-               "なんでも継続", "taporobo", "トランポリン LISP continuation",
-               1187568692.98765)
-p add_bookmark("http://jp.rubyist.net/managinze",
-               "るびま", "moritan", "Ruby ドキュメント",
-               Time.now)
-p add_bookmark("http://jp.rubyist.net/managinze",
-               "るびま", "taporobo", "Ruby 雑誌",
-               Time.now)
-p add_bookmark("http://groonga.rubyforge.org/",
-               "ラングバ", "moritan", "Ruby groonga",
-               Time.parse("2009-07-19"))
+p(add_bookmark("http://jp.rubyist.net/magazine/",
+               "Rubyist Magazine - るびま", "moritan", "Ruby 記事",
+               Time.parse("2010-10-07T14:18:28+09:00")))
+p(add_bookmark("http://groonga.rubyforge.org/",
+               "Rubyでgroonga使って全文検索 - ラングバ", "taporobo",
+               "Ruby groonga 全文検索",
+               Time.parse("2010-11-11T12:39:59+09:00")))
+p(add_bookmark("http://www.rubyist.net/~matz/",
+               "Matz日記", "taporobo", "Ruby 日記",
+               Time.parse("2010-07-28T20:46:23+09:00")))
 
-
-records = comments.select do |record|
-  record["content"] =~ "LISP"
-end
+p(records = comments.select do |record|
+    record["content"] =~ "Ruby"
+  end)
 
 records.each do |record|
   record = record.key
   p [record.id,
-     record[".issued"],
-     record[".item.title"],
-     record[".author.name"],
-     record[".content"]]
+     record.issued,
+     record.item.title,
+     record.author.name,
+     record.content]
 end
 
 p records
 
-records.sort([{:key => ".issued", :order => "descending"}]).each do |record|
+records.sort([{:key => "issued", :order => "descending"}]).each do |record|
   record = record.key
   p [record.id,
-     record[".issued"],
-     record[".item.title"],
-     record[".author.name"],
-     record[".content"]]
+     record.issued,
+     record.item.title,
+     record.author.name,
+     record.content]
 end
 
 records.group("item").each do |record|
   item = record.key
   p [record.n_sub_records,
      item.key,
-     item[".title"]]
+     item.title]
 end
 
-p ruby_comments = @comments.select {|record| record["content"] =~ "Ruby"}
-p ruby_items = @items.select("*W1:50 title:@Ruby")
+p(ruby_comments = @comments.select {|record| record.content =~ "Ruby"})
+p(ruby_items = @items.select do |record|
+    target = record.match_target do |match_record|
+      match_record.title * 10
+    end
+    target =~ "Ruby"
+  end)
 
-p ruby_items = ruby_comments.group(".item").union!(ruby_items)
-ruby_items.sort([{:key => "._score", :order => "descending"}]).each do |record|
-  p [record["._score"], record[".title"]]
+p(ruby_items = ruby_comments.group("item").union!(ruby_items))
+ruby_items.sort([{:key => "_score", :order => "descending"}]).each do |record|
+  p [record.score, record.title]
 end
