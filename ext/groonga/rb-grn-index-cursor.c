72b3ea437b6d79a7630823759a7a25e2794d0c0f
yoshihara haruka <yoshihara@clear-code.com>
2011-05-17 15:20:17 +0900

    created macro to translate for grn_index_cursor to ruby_object.

--- ext/groonga/rb-grn-index-cursor.c
+++ ext/groonga/rb-grn-index-cursor.c
@@ -2,6 +2,14 @@
 
 VALUE rb_cGrnIndexCursor;
 
+VALUE
+rb_grn_index_cursor_to_ruby_object (grn_ctx *context,
+				    grn_obj *cursor,
+				    grn_bool owner)
+{
+  return GRNOBJECT2RVAL(rb_cGrnIndexCursor, context, cursor, owner);
+}
+
 void
 rb_grn_init_index_cursor (VALUE mGrn)
 {
