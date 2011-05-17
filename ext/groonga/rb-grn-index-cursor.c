f03f8426df91f45f6d3e78e2c618b8b936852339
yoshihara haruka <yoshihara@clear-code.com>
2011-05-17 17:25:16 +0900

    added IndexCursor#next.

--- ext/groonga/rb-grn-index-cursor.c
+++ ext/groonga/rb-grn-index-cursor.c
@@ -1,4 +1,24 @@
+/* -*- c-file-style: "ruby" -*- */
+/*
+  Copyright (C) 2011  Haruka Yoshihara <yoshihara@clear-code.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include "rb-grn.h"
+
+#define SELF(object) ((RbGrnIndexCursor *)DATA_PTR(object))
 
 VALUE rb_cGrnIndexCursor;
 
@@ -7,12 +27,56 @@ rb_grn_index_cursor_to_ruby_object (grn_ctx *context,
 				    grn_obj *cursor,
 				    grn_bool owner)
 {
-  return GRNOBJECT2RVAL(rb_cGrnIndexCursor, context, cursor, owner);
+    return GRNOBJECT2RVAL(rb_cGrnIndexCursor, context, cursor, owner);
+}
+
+void
+rb_grn_index_cursor_deconstruct (RbGrnIndexCursor *rb_grn_index_cursor,
+				 grn_obj **cursor,
+				 grn_ctx **context,
+				 grn_id *domain_id,
+				 grn_obj **domain,
+				 grn_id *range_id,
+				 grn_obj **range)
+{
+    RbGrnObject *rb_grn_object;
+
+    rb_grn_object = RB_GRN_OBJECT(rb_grn_index_cursor);
+    rb_grn_object_deconstruct(rb_grn_object, cursor, context,
+			      domain_id, domain,
+			      range_id, range);
+}
+
+static VALUE
+rb_grn_index_cursor_next (VALUE self)
+{
+    VALUE rb_posting = Qnil;
+    grn_obj *cursor;
+    grn_ctx *context;
+
+    rb_grn_index_cursor_deconstruct(SELF(self), &cursor, &context,
+				    NULL, NULL, NULL, NULL);
+
+    if (context && cursor) {
+	grn_posting *posting;
+	grn_id tid;
+
+	posting = grn_index_cursor_next(context, cursor, &tid);
+
+	if (posting) {
+	    rb_posting = rb_grn_posting_new(posting, tid);
+	}
+    }
+
+    return rb_posting;
+
 }
 
 void
 rb_grn_init_index_cursor (VALUE mGrn)
 {
-  rb_cGrnIndexCursor = rb_define_class_under(mGrn, "IndexCursor", rb_cObject);
-  rb_define_alloc_func(rb_cGrnIndexCursor, rb_grn_object_alloc);
+    rb_cGrnIndexCursor = rb_define_class_under(mGrn, "IndexCursor", rb_cObject);
+    rb_define_alloc_func(rb_cGrnIndexCursor, rb_grn_object_alloc);
+    rb_define_method(rb_cGrnIndexCursor, "next", rb_grn_index_cursor_next, 0);
+
 }
--- test/test-index-cursor.rb
+++ test/test-index-cursor.rb
@@ -2,11 +2,38 @@ class IndexCursorTest < Test::Unit::TestCase
   include GroongaTestUtils
 
   def setup
+    setup_database
   end
 
-  def test_initialize
-    assert_nothing_raised do
-      Groonga::IndexCursor.new
+  def test_open_cursor
+    Groonga::Schema.define do |schema|
+      schema.create_table("Articles") do |table|
+        table.text("content")
+      end
+
+      schema.create_table("Terms",
+                          :type => :hash,
+                          :default_tokenizer => :bigram_split_symbol_alpha) do |table|
+        table.index("Articles.content")
+      end
+    end
+
+    articles = Groonga["Articles"]
+    terms = Groonga["Terms"]
+    content_index = Groonga["Terms.Articles_content"]
+
+    articles.add(:content => "l")
+    articles.add(:content => "ll")
+    articles.add(:content => "hello")
+
+    terms.open_cursor do |table_cursor|
+      cursor = content_index.open_cursor(table_cursor)
+      postings = []
+      while (posting = cursor.next)
+        postings << posting
+      end
+      assert_equal(["xxx"],
+                   postings)
     end
   end
 end
