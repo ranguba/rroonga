d29264e84f221ce49cb5018838e1dc7ab4317f0a
yoshihara haruka <yoshihara@clear-code.com>
2011-05-23 16:13:35 +0900

    created create_hash method for test_cursor_open.

--- test/test-index-cursor.rb
+++ test/test-index-cursor.rb
@@ -26,97 +26,42 @@ class IndexCursorTest < Test::Unit::TestCase
     articles.add(:content => "ll")
     articles.add(:content => "hello")
 
+    postings = []
     terms.open_cursor do |table_cursor|
       cursor = content_index.open_cursor(table_cursor)
-      postings = []
       while (posting = cursor.next)
         postings << posting
-
-        # term = Groonga::Record.new(terms, posting.term_id)
-        # p [posting, term.key]
-        # record = Groonga::Record.new(articles, posting.record_id)
-        # p [posting.record_id, record["content"]]
       end
-
-      assert_equal(1, postings[0].record_id)
-      assert_equal(1, postings[0].section_id)
-      assert_equal(1, postings[0].term_id)
-      assert_equal(0, postings[0].position)
-      assert_equal(1, postings[0].term_frequency)
-      assert_equal(0, postings[0].weight)
-      assert_equal(1, postings[0].n_rest_postings)
-
-      assert_equal(2, postings[1].record_id)
-      assert_equal(1, postings[1].section_id)
-      assert_equal(1, postings[1].term_id)
-      assert_equal(0, postings[1].position)
-      assert_equal(1, postings[1].term_frequency)
-      assert_equal(0, postings[1].weight)
-      assert_equal(1, postings[1].n_rest_postings)
-
-      assert_equal(2, postings[2].record_id)
-      assert_equal(1, postings[2].section_id)
-      assert_equal(2, postings[2].term_id)
-      assert_equal(0, postings[2].position)
-      assert_equal(1, postings[2].term_frequency)
-      assert_equal(0, postings[2].weight)
-      assert_equal(1, postings[2].n_rest_postings)
-
-      assert_equal(3, postings[3].record_id)
-      assert_equal(1, postings[3].section_id)
-      assert_equal(2, postings[3].term_id)
-      assert_equal(0, postings[3].position)
-      assert_equal(1, postings[3].term_frequency)
-      assert_equal(0, postings[3].weight)
-      assert_equal(1, postings[3].n_rest_postings)
-
-      assert_equal(3, postings[4].record_id)
-      assert_equal(1, postings[4].section_id)
-      assert_equal(3, postings[4].term_id)
-      assert_equal(0, postings[4].position)
-      assert_equal(1, postings[4].term_frequency)
-      assert_equal(0, postings[4].weight)
-      assert_equal(0, postings[4].n_rest_postings)
-
-
-      assert_equal({
-                     :record_id => 3,
-                     :section_id => 1,
-                     :term_id => 4,
-                     :postion => 1,
-                     :term_frequency => 1,
-                     :weight => 0,
-                     :n_rest_postings => 0
-                   },
-                   postings[5].to_hash)
-      assert_equal({
-                     :record_id => 3,
-                     :section_id => 1,
-                     :term_id => 5,
-                     :postion => 3,
-                     :term_frequency => 1,
-                     :weight => 0,
-                     :n_rest_postings => 0
-                   },
-                   postings[6].to_hash)
-
-
-
-      assert_equal({:record_id => 3,
-                     :section_id => 1,
-                     :term_id => 6,
-                     :postion => 4,
-                     :term_frequency => 1,
-                     :weight => 0,
-                     :n_rest_postings => 0},
-                   {:record_id => postings[7].record_id,
-                     :section_id => postings[7].section_id,
-                     :term_id => postings[7].term_id,
-                     :postion => postings[7].position,
-                     :term_frequency => postings[7].term_frequency,
-                     :weight => postings[7].weight,
-                     :n_rest_postings => postings[7].n_rest_postings})
-
     end
+
+    parameters =
+      [:record_id, :section_id, :term_id, :position,
+       :term_frequency, :weight, :n_rest_postings]
+    expected =
+      [
+       [1, 1, 1, 0, 1, 0, 1],
+       [2, 1, 1, 0, 1, 0, 1],
+       [2, 1, 2, 0, 1, 0, 1],
+       [3, 1, 2, 0, 1, 0, 1],
+       [3, 1, 3, 0, 1, 0, 0],
+       [3, 1, 4, 1, 1, 0, 0],
+       [3, 1, 5, 3, 1, 0, 0],
+       [3, 1, 6, 4, 1, 0, 0]
+      ]
+    assert_equal(create_hashes(parameters, expected),
+                 postings.collect {|posting| posting.to_hash})
+  end
+
+  private
+  def create_hashes(keys, values)
+    hashes = []
+    values.each do |value|
+      hash = {}
+      keys.each_with_index do |key, i|
+        hash[key] = value[i]
+      end
+      hashes << hash
+    end
+    hashes
   end
 end
