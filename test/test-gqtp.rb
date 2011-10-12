b923641f3e39246e4be31fae6812456a791990f6
Kouhei Sutou <kou@clear-code.com>
2011-10-12 14:17:01 +0900

    [test] remove an unused variable.

--- test/test-gqtp.rb
+++ test/test-gqtp.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2010-2011  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -64,7 +64,7 @@ class GQTPTest < Test::Unit::TestCase
   private
   def process(gqtp)
     context.send(gqtp)
-    id, result = context.receive
+    _, result = context.receive
     JSON.parse(result)
   end
 end
