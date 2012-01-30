7e346a0941d95f63995bddea5a6cdda76c4ae8a0
Kouhei Sutou <kou@clear-code.com>
2012-01-30 16:27:40 +0900

    [test] add workaround for GC and expression.

--- test/test-table-select.rb
+++ test/test-table-select.rb
@@ -53,27 +53,37 @@ class TableSelectTest < Test::Unit::TestCase
                     :user => "darashi")
   end
 
+  setup
+  def setup_variables
+    @result = nil
+  end
+
+  teardown
+  def teardown_expression_close
+    @result.expression.close if @result # TODO: We want to remove it.
+  end
+
   def test_sub_expression
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record.match("Hello", "content") &
         (record["created_at"] < Time.parse("2009-08-01"))
     end
-    assert_equal_select_result([@comment2], result)
+    assert_equal_select_result([@comment2], @result)
   end
 
   def test_query
-    result = @comments.select("content:@Hello")
-    assert_equal_select_result([@comment1, @comment2], result)
+    @result = @comments.select("content:@Hello")
+    assert_equal_select_result([@comment1, @comment2], @result)
   end
 
   def test_query_with_parser
-    result = @comments.select("content @ \"Hello\"", :syntax => :script)
-    assert_equal_select_result([@comment1, @comment2], result)
+    @result = @comments.select("content @ \"Hello\"", :syntax => :script)
+    assert_equal_select_result([@comment1, @comment2], @result)
   end
 
   def test_query_with_default_column
-    result = @comments.select("Hello", {:default_column => 'content'})
-    assert_equal_select_result([@comment1, @comment2], result)
+    @result = @comments.select("Hello", {:default_column => 'content'})
+    assert_equal_select_result([@comment1, @comment2], @result)
   end
 
   def test_expression
@@ -82,104 +92,105 @@ class TableSelectTest < Test::Unit::TestCase
     expression.append_object(variable)
     expression.parse("content:@Hello", :syntax => :query)
     expression.compile
-    result = @comments.select(expression)
-    assert_equal(expression, result.expression)
-    assert_equal_select_result([@comment1, @comment2], result)
+    @result = @comments.select(expression)
+    assert_equal(expression, @result.expression)
+    assert_equal_select_result([@comment1, @comment2], @result)
   end
 
   def test_query_with_block
-    result = @comments.select("content:@Hello") do |record|
+    @result = @comments.select("content:@Hello") do |record|
       record["created_at"] < Time.parse("2009-08-01")
     end
-    assert_equal_select_result([@comment2], result)
+    assert_equal_select_result([@comment2], @result)
   end
 
   def test_query_with_block_match
-    result = @comments.select("content:@Hello") do |record|
+    @result = @comments.select("content:@Hello") do |record|
       record.match("World", "content")
     end
-    assert_equal_select_result([@comment2], result)
+    assert_equal_select_result([@comment2], @result)
   end
 
   def test_without_block
+    @result = @comments.select
     assert_equal_select_result([@comment1, @comment2,
                                 @comment3, @japanese_comment],
-                               @comments.select)
+                               @result)
   end
 
   def test_query_japanese
-    result = @comments.select("content:@ボロTV")
-    assert_equal_select_result([@japanese_comment], result)
+    @result = @comments.select("content:@ボロTV")
+    assert_equal_select_result([@japanese_comment], @result)
   end
 
   def test_but_query
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record["content"].match "Hello -World"
     end
-    assert_equal_select_result([@comment1], result)
+    assert_equal_select_result([@comment1], @result)
   end
 
   def test_query_with_three_terms
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record["content"].match "Say Hello World"
     end
-    assert_equal_select_result([], result)
+    assert_equal_select_result([], @result)
   end
 
   def test_query_with_brackets
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record["content"].match "Say (Hello World)"
     end
-    assert_equal_select_result([], result)
+    assert_equal_select_result([], @result)
   end
 
   def test_equal_reference_column_by_key
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record["user"] == "darashi"
     end
-    assert_equal_select_result([@japanese_comment], result)
+    assert_equal_select_result([@japanese_comment], @result)
   end
 
   def test_not_equal_reference_column_by_key
-    result = @comments.select('user != "darashi"', :syntax => :script)
+    @result = @comments.select('user != "darashi"', :syntax => :script)
     assert_equal_select_result([@comment1, @comment2, @comment3],
-                               result)
+                               @result)
   end
 
   def test_not_equal_block
     only_ruby19
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record.user != "darashi"
     end
     assert_equal_select_result([@comment1, @comment2, @comment3],
-                               result)
+                               @result)
   end
 
   def test_equal_reference_column_by_nonexistent_key
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record["user"] == "nonexistent"
     end
-    assert_equal_select_result([], result)
+    assert_equal_select_result([], @result)
   end
 
   def test_query_key
-    result = @users.select do |record|
+    @result = @users.select do |record|
       record["_key"] =~ "mori"
     end
-    assert_equal_select_result([@users["morita"]], result)
+    assert_equal_select_result([@users["morita"]], @result)
   end
 
   def test_symbol
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record[:content].match("Say Hello World")
     end
-    assert_equal_select_result([], result)
+    assert_equal_select_result([], @result)
   end
 
   def test_method
-    result = @comments.select do |record|
+    @result = @comments.select do |record|
       record.content.match("Say Hello World")
     end
-    assert_equal_select_result([], result)
+    assert_equal_select_result([], @result)
   end
 end
