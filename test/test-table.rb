33d750bff9481be84cd72600091bda4e905547d7
Ryo Onodera <onodera@clear-code.com>
2012-05-23 17:58:00 +0900

    Use column name instead of column object

--- test/test-table.rb
+++ test/test-table.rb
@@ -192,8 +192,8 @@ class TableTest < Test::Unit::TestCase
   def test_add_vector_column_referencing_to_normalized_table_indexed_via_column_value
     people = Groonga::Hash.create(:name => "People", :key_normalize => true)
     movies = Groonga::Hash.create(:name => "Movies")
-    casts = movies.define_column("casts", people, :type => :vector)
-    people.define_index_column("index", movies, casts)
+    movies.define_column("casts", people, :type => :vector)
+    people.define_index_column("index", movies, "casts")
 
     movies.add("DOCUMENTARY of AKB48", :casts => ["AKB48"])
 
