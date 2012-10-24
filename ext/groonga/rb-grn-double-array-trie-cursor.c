bce2d4fe0eb3a041cb71b5aa91d86c67ed764000
Haruka Yoshihara <yoshihara@clear-code.com>
2012-10-24 16:18:18 +0900

    doc: fix Class name
    Groonga::DoubleArrayCursor ->
    Groonga::DoubleArrayTrieCursor

--- ext/groonga/rb-grn-double-array-trie-cursor.c
+++ ext/groonga/rb-grn-double-array-trie-cursor.c
@@ -21,7 +21,7 @@
 VALUE rb_cGrnDoubleArrayTrieCursor;
 
 /*
- * Document-class: Groonga::DoubleArrayCursor < Groonga::TableCursor
+ * Document-class: Groonga::DoubleArrayTrieCursor < Groonga::TableCursor
  *
  * {Groonga::DoubleArrayTrie} に登録されているレコードを順番に取り
  * 出すためのオブジェクト。利用できるメソッドは
