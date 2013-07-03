132da001f6a427afb3c98721a655e9ac30a03da0
Kouhei Sutou <kou@clear-code.com>
2013-07-03 21:55:04 +0900

    Use " (double quote) for string literal

--- lib/groonga/record.rb
+++ lib/groonga/record.rb
@@ -15,7 +15,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require 'English'
+require "English"
 require "time"
 
 require "groonga/sub-records"
