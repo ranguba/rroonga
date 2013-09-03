9f6eed9c812484980975e836de59034b20feff97
Kouhei Sutou <kou@clear-code.com>
2013-09-03 15:48:21 +0900

    test: indent

--- test/test-index-column.rb
+++ test/test-index-column.rb
@@ -53,167 +53,167 @@ class IndexColumnTest < Test::Unit::TestCase
   end
 
   class CRUDTest < self
-  def test_array_set_with_record
-    articles = Groonga::Array.create(:name => "Articles")
-    articles.define_column("content", "Text")
+    def test_array_set_with_record
+      articles = Groonga::Array.create(:name => "Articles")
+      articles.define_column("content", "Text")
 
-    terms = Groonga::Hash.create(:name => "Terms",
-                                 :default_tokenizer => "TokenBigram")
-    content_index = terms.define_index_column("content", articles,
-                                              :with_section => true)
+      terms = Groonga::Hash.create(:name => "Terms",
+                                   :default_tokenizer => "TokenBigram")
+      content_index = terms.define_index_column("content", articles,
+                                                :with_section => true)
 
-    content = <<-EOC
-    groonga は組み込み型の全文検索エンジンライブラリです。
-    DBMSやスクリプト言語処理系等に組み込むことによって、その
-    全文検索機能を強化することができます。また、リレーショナ
-    ルモデルに基づくデータストア機能を内包しており、groonga
-    単体でも高速なデータストアサーバとして使用することができ
-    ます。
+      content = <<-EOC
+      groonga は組み込み型の全文検索エンジンライブラリです。
+      DBMSやスクリプト言語処理系等に組み込むことによって、その
+      全文検索機能を強化することができます。また、リレーショナ
+      ルモデルに基づくデータストア機能を内包しており、groonga
+      単体でも高速なデータストアサーバとして使用することができ
+      ます。
 
-    ■全文検索方式
-    転置索引型の全文検索エンジンです。転置索引は圧縮されてファ
-    イルに格納され、検索時のディスク読み出し量を小さく、かつ
-    局所的に抑えるように設計されています。用途に応じて以下の
-    索引タイプを選択できます。
-    EOC
+      ■全文検索方式
+      転置索引型の全文検索エンジンです。転置索引は圧縮されてファ
+      イルに格納され、検索時のディスク読み出し量を小さく、かつ
+      局所的に抑えるように設計されています。用途に応じて以下の
+      索引タイプを選択できます。
+      EOC
 
-    groonga = articles.add(:content => content)
+      groonga = articles.add(:content => content)
 
-    content.split(/\n{2,}/).each_with_index do |sentence, i|
-      content_index[groonga] = {:value => sentence, :section => i + 1}
+      content.split(/\n{2,}/).each_with_index do |sentence, i|
+        content_index[groonga] = {:value => sentence, :section => i + 1}
+      end
+      assert_equal([groonga],
+                   content_index.search("エンジン").collect {|record| record.key})
     end
-    assert_equal([groonga],
-                 content_index.search("エンジン").collect {|record| record.key})
-  end
 
-  def test_add
-    articles = Groonga::Array.create(:name => "Articles")
-    articles.define_column("content", "Text")
+    def test_add
+      articles = Groonga::Array.create(:name => "Articles")
+      articles.define_column("content", "Text")
 
-    terms = Groonga::Hash.create(:name => "Terms",
-                                 :key_type => "ShortText",
-                                 :default_tokenizer => "TokenBigram")
-    content_index = terms.define_index_column("content", articles,
-                                              :with_position => true,
-                                              :with_section => true)
+      terms = Groonga::Hash.create(:name => "Terms",
+                                   :key_type => "ShortText",
+                                   :default_tokenizer => "TokenBigram")
+      content_index = terms.define_index_column("content", articles,
+                                                :with_position => true,
+                                                :with_section => true)
 
-    content = <<-CONTENT
-    Groonga is a fast and accurate full text search engine based on
-    inverted index. One of the characteristics of groonga is that a
-    newly registered document instantly appears in search
-    results. Also, groonga allows updates without read locks. These
-    characteristics result in superior performance on real-time
-    applications.
+      content = <<-CONTENT
+      Groonga is a fast and accurate full text search engine based on
+      inverted index. One of the characteristics of groonga is that a
+      newly registered document instantly appears in search
+      results. Also, groonga allows updates without read locks. These
+      characteristics result in superior performance on real-time
+      applications.
 
-    Groonga is also a column-oriented database management system
-    (DBMS). Compared with well-known row-oriented systems, such as
-    MySQL and PostgreSQL, column-oriented systems are more suited for
-    aggregate queries. Due to this advantage, groonga can cover
-    weakness of row-oriented systems.
+      Groonga is also a column-oriented database management system
+      (DBMS). Compared with well-known row-oriented systems, such as
+      MySQL and PostgreSQL, column-oriented systems are more suited for
+      aggregate queries. Due to this advantage, groonga can cover
+      weakness of row-oriented systems.
 
-    The basic functions of groonga are provided in a C library. Also,
-    libraries for using groonga in other languages, such as Ruby, are
-    provided by related projects. In addition, groonga-based storage
-    engines are provided for MySQL and PostgreSQL. These libraries
-    and storage engines allow any application to use groonga. See
-    usage examples.
-    CONTENT
+      The basic functions of groonga are provided in a C library. Also,
+      libraries for using groonga in other languages, such as Ruby, are
+      provided by related projects. In addition, groonga-based storage
+      engines are provided for MySQL and PostgreSQL. These libraries
+      and storage engines allow any application to use groonga. See
+      usage examples.
+      CONTENT
 
-    groonga = articles.add(:content => content)
+      groonga = articles.add(:content => content)
 
-    content.split(/\n{2,}/).each_with_index do |sentence, i|
-      content_index.add(groonga, sentence, :section => i + 1)
+      content.split(/\n{2,}/).each_with_index do |sentence, i|
+        content_index.add(groonga, sentence, :section => i + 1)
+      end
+      assert_equal([groonga], content_index.search("engine").collect(&:key))
     end
-    assert_equal([groonga], content_index.search("engine").collect(&:key))
-  end
 
-  def test_delete
-    articles = Groonga::Array.create(:name => "Articles")
-    articles.define_column("content", "Text")
+    def test_delete
+      articles = Groonga::Array.create(:name => "Articles")
+      articles.define_column("content", "Text")
 
-    terms = Groonga::Hash.create(:name => "Terms",
-                                 :key_type => "ShortText",
-                                 :default_tokenizer => "TokenBigram")
-    content_index = terms.define_index_column("content", articles,
-                                              :with_position => true,
-                                              :with_section => true)
+      terms = Groonga::Hash.create(:name => "Terms",
+                                   :key_type => "ShortText",
+                                   :default_tokenizer => "TokenBigram")
+      content_index = terms.define_index_column("content", articles,
+                                                :with_position => true,
+                                                :with_section => true)
 
-    content = <<-CONTENT
-    Groonga is a fast and accurate full text search engine based on
-    inverted index. One of the characteristics of groonga is that a
-    newly registered document instantly appears in search
-    results. Also, groonga allows updates without read locks. These
-    characteristics result in superior performance on real-time
-    applications.
+      content = <<-CONTENT
+      Groonga is a fast and accurate full text search engine based on
+      inverted index. One of the characteristics of groonga is that a
+      newly registered document instantly appears in search
+      results. Also, groonga allows updates without read locks. These
+      characteristics result in superior performance on real-time
+      applications.
 
-    Groonga is also a column-oriented database management system
-    (DBMS). Compared with well-known row-oriented systems, such as
-    MySQL and PostgreSQL, column-oriented systems are more suited for
-    aggregate queries. Due to this advantage, groonga can cover
-    weakness of row-oriented systems.
+      Groonga is also a column-oriented database management system
+      (DBMS). Compared with well-known row-oriented systems, such as
+      MySQL and PostgreSQL, column-oriented systems are more suited for
+      aggregate queries. Due to this advantage, groonga can cover
+      weakness of row-oriented systems.
 
-    The basic functions of groonga are provided in a C library. Also,
-    libraries for using groonga in other languages, such as Ruby, are
-    provided by related projects. In addition, groonga-based storage
-    engines are provided for MySQL and PostgreSQL. These libraries
-    and storage engines allow any application to use groonga. See
-    usage examples.
-    CONTENT
+      The basic functions of groonga are provided in a C library. Also,
+      libraries for using groonga in other languages, such as Ruby, are
+      provided by related projects. In addition, groonga-based storage
+      engines are provided for MySQL and PostgreSQL. These libraries
+      and storage engines allow any application to use groonga. See
+      usage examples.
+      CONTENT
 
-    groonga = articles.add(:content => content)
+      groonga = articles.add(:content => content)
 
-    content.split(/\n{2,}/).each_with_index do |sentence, i|
-      content_index.add(groonga, sentence, :section => i + 1)
+      content.split(/\n{2,}/).each_with_index do |sentence, i|
+        content_index.add(groonga, sentence, :section => i + 1)
+      end
+      content.split(/\n{2,}/).each_with_index do |sentence, i|
+        content_index.delete(groonga, sentence, :section => i + 1)
+      end
+      assert_equal([], content_index.search("engine").collect(&:key))
     end
-    content.split(/\n{2,}/).each_with_index do |sentence, i|
-      content_index.delete(groonga, sentence, :section => i + 1)
+
+    def test_update
+      articles = Groonga::Array.create(:name => "Articles")
+      articles.define_column("content", "Text")
+
+      terms = Groonga::Hash.create(:name => "Terms",
+                                   :key_type => "ShortText",
+                                   :default_tokenizer => "TokenBigram")
+      content_index = terms.define_index_column("content", articles,
+                                                :with_position => true,
+                                                :with_section => true)
+
+      old_sentence = <<-SENTENCE
+      Groonga is a fast and accurate full text search engine based on
+      inverted index. One of the characteristics of groonga is that a
+      newly registered document instantly appears in search
+      results. Also, groonga allows updates without read locks. These
+      characteristics result in superior performance on real-time
+      applications.
+      SENTENCE
+
+      new_sentence = <<-SENTENCE
+      Groonga is also a column-oriented database management system
+      (DBMS). Compared with well-known row-oriented systems, such as
+      MySQL and PostgreSQL, column-oriented systems are more suited for
+      aggregate queries. Due to this advantage, groonga can cover
+      weakness of row-oriented systems.
+      SENTENCE
+
+      groonga = articles.add(:content => old_sentence)
+
+      content_index.add(groonga, old_sentence, :section => 1)
+      assert_equal([groonga],
+                   content_index.search("engine").collect(&:key))
+      assert_equal([],
+                   content_index.search("MySQL").collect(&:key))
+
+      groonga[:content] = new_sentence
+      content_index.update(groonga, old_sentence, new_sentence, :section => 1)
+      assert_equal([],
+                   content_index.search("engine").collect(&:key))
+      assert_equal([groonga],
+                   content_index.search("MySQL").collect(&:key))
     end
-    assert_equal([], content_index.search("engine").collect(&:key))
-  end
-
-  def test_update
-    articles = Groonga::Array.create(:name => "Articles")
-    articles.define_column("content", "Text")
-
-    terms = Groonga::Hash.create(:name => "Terms",
-                                 :key_type => "ShortText",
-                                 :default_tokenizer => "TokenBigram")
-    content_index = terms.define_index_column("content", articles,
-                                              :with_position => true,
-                                              :with_section => true)
-
-    old_sentence = <<-SENTENCE
-    Groonga is a fast and accurate full text search engine based on
-    inverted index. One of the characteristics of groonga is that a
-    newly registered document instantly appears in search
-    results. Also, groonga allows updates without read locks. These
-    characteristics result in superior performance on real-time
-    applications.
-    SENTENCE
-
-    new_sentence = <<-SENTENCE
-    Groonga is also a column-oriented database management system
-    (DBMS). Compared with well-known row-oriented systems, such as
-    MySQL and PostgreSQL, column-oriented systems are more suited for
-    aggregate queries. Due to this advantage, groonga can cover
-    weakness of row-oriented systems.
-    SENTENCE
-
-    groonga = articles.add(:content => old_sentence)
-
-    content_index.add(groonga, old_sentence, :section => 1)
-    assert_equal([groonga],
-                 content_index.search("engine").collect(&:key))
-    assert_equal([],
-                 content_index.search("MySQL").collect(&:key))
-
-    groonga[:content] = new_sentence
-    content_index.update(groonga, old_sentence, new_sentence, :section => 1)
-    assert_equal([],
-                 content_index.search("engine").collect(&:key))
-    assert_equal([groonga],
-                 content_index.search("MySQL").collect(&:key))
-  end
   end
 
   def test_shorter_query_than_ngram
