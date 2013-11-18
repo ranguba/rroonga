d2542e922c040113a3935e5a9113b7e3df703370
Kouhei Sutou <kou@clear-code.com>
2013-11-18 15:44:02 +0900

    Use " (double quote) for string literal

--- test/groonga-test-utils.rb
+++ test/groonga-test-utils.rb
@@ -13,16 +13,16 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require 'fileutils'
-require 'pathname'
-require 'tempfile'
-require 'time'
-require 'erb'
-require 'stringio'
-require 'json'
-require 'pkg-config'
+require "fileutils"
+require "pathname"
+require "tempfile"
+require "time"
+require "erb"
+require "stringio"
+require "json"
+require "pkg-config"
 
-require 'groonga'
+require "groonga"
 
 module GroongaTestUtils
   class << self
