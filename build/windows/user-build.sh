78814fc12a42bc0d043f9f330b16d3c69771105b
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-09-27 17:26:57 +0900

    windows: use Ruby 2.1.3

--- build/windows/user-build.sh
+++ build/windows/user-build.sh
@@ -2,8 +2,8 @@
 
 readonly RUBY19="1.9.3-p547"
 readonly RUBY20="2.0.0-p481"
-readonly RUBY21="2.1.2"
-readonly CROSS_TERGET="1.9.3:2.0.0:2.1.2"
+readonly RUBY21="2.1.3"
+readonly CROSS_TERGET="1.9.3:2.0.0:2.1.3"
 readonly PKG_GEMS="/vagrant/pkg"
 readonly CPU_NUMS=`cat /proc/cpuinfo |grep processor|wc -l`
 
