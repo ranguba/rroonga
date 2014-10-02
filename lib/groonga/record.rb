ranguba/rroonga

commit 9b23b1ef21195847fe0e687cb660552f4027f87d

Add missing `:id => true` option
It follows the change by commit 06e06f7ef5b754ee65f34169108e4631f1dad300

lib/groonga/record.rb
@@ -279,9 +279,9 @@ module Groonga
       as_json.to_json(*args)
     end
 
-    # レコードを削除する。
+    # Delete the record.
     def delete
-      @table.delete(@id)
+      @table.delete(@id, :id => true)
     end
 
     # Renames key of the record. Only available for

commit 7773498759e615bbf986da1401c99ee3a5c34aae

Fix a bug that Groonga::Array#delete fails
Caused by commit 9b23b1ef21195847fe0e687cb660552f4027f87d

lib/groonga/record.rb
@@ -281,7 +281,11 @@ module Groonga
 
     # Delete the record.
     def delete
+      if @table.is_a?(Groonga::Table::KeySupport)
       @table.delete(@id, :id => true)
+      else
+        @table.delete(@id)
+      end
     end
 
     # Renames key of the record. Only available for