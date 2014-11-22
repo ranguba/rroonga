b3e78313700b8add2239d3d39080925b40e5d991
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-22 19:26:00 +0900

    windows: use latest Ruby 2.0

--- build/windows/user-build.sh
+++ build/windows/user-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 readonly RUBY19="1.9.3-p551"
-readonly RUBY20="2.0.0-p576"
+readonly RUBY20="2.0.0-p598"
 readonly RUBY21="2.1.3"
 readonly CROSS_TERGET="1.9.3:2.0.0:2.1.3"
 readonly PKG_GEMS="/vagrant/pkg"
