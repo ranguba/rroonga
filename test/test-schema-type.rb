980ba27cf72604ea879b5fe63d24930dcb51fb54
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-02 17:31:31 +0900

    test: split tests

--- test/test-schema-type.rb
+++ test/test-schema-type.rb
@@ -201,14 +201,22 @@ class SchemaTypeTest < Test::Unit::TestCase
     assert_normalize_type("TokenMecab", "TokenMecab")
   end
 
-  def test_normalize_regexp
-    assert_normalize_type("TokenRegexp", "regexp")
-    assert_normalize_type("TokenRegexp", "token_regexp")
-    assert_normalize_type("TokenRegexp", "TokenRegexp")
-  end
-
   private
   def assert_normalize_type(expected, type)
     assert_equal(expected, Groonga::Schema.normalize_type(type))
+  end
+
+  class RegexpTest < self
+    def test_normalize_short_name
+      assert_normalize_type("TokenRegexp", "regexp")
+    end
+
+    def test_normalize_snake_case
+      assert_normalize_type("TokenRegexp", "token_regexp")
+    end
+
+    def test_normalize_camel_case
+      assert_normalize_type("TokenRegexp", "TokenRegexp")
+    end
   end
 end
