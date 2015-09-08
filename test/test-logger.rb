1a8f732577b855ea7105ed550d59e9d013150505
Kouhei Sutou <kou@clear-code.com>
2015-09-08 18:41:19 +0900

    Revert "test: group"
    This reverts commit d13603f31397f0f96a603bcc63d055a1fd5ba742.
    
    It's needless grouping.

--- test/test-logger.rb
+++ test/test-logger.rb
@@ -37,12 +37,10 @@ class LoggerTest < Test::Unit::TestCase
     assert_true(@log_path.exist?)
   end
 
-  sub_test_case ".max_level" do
-    test "default" do
-      assert_equal(:notice, Groonga::Logger.max_level)
-      Groonga::Logger.max_level = :debug
-      assert_equal(:debug, Groonga::Logger.max_level)
-    end
+  def test_max_level
+    assert_equal(:notice, Groonga::Logger.max_level)
+    Groonga::Logger.max_level = :debug
+    assert_equal(:debug, Groonga::Logger.max_level)
   end
 
   sub_test_case ".log" do
