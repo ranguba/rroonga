08f9b363a9d639da27a5522f013be6eb9f401856
Kouhei Sutou <kou@clear-code.com>
2015-10-06 22:08:48 +0900

    Remove unused files

--- build/windows/cross-build-rroonga.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get -y install libssl-dev libreadline-dev
-sudo apt-get -y install git build-essential ruby mingw-w64
-
-/vagrant/user-build.sh
