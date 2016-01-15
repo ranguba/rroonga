ac0e322a43870a043c49839c2e717f0a7da8ac28
Kouhei Sutou <kou@clear-code.com>
2016-01-15 21:09:52 +0900

    test: stop to use duplicated name

--- test/test-package-label.rb
+++ test/test-package-label.rb
@@ -14,7 +14,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 class PackageLabelTest < Test::Unit::TestCase
-  def test_package_label
+  def test_reader
     assert_equal("Groonga", Groonga.package_label)
   end
 end
