4aa2b060b6d09a51ee7187d285cfb9b8a6a3d9d7
Kouhei Sutou <kou@clear-code.com>
2016-08-13 12:26:02 +0900

    Bump version

--- ext/groonga/rb-grn.h
+++ ext/groonga/rb-grn.h
@@ -99,7 +99,7 @@ RB_GRN_BEGIN_DECLS
 
 #define RB_GRN_MAJOR_VERSION 6
 #define RB_GRN_MINOR_VERSION 0
-#define RB_GRN_MICRO_VERSION 7
+#define RB_GRN_MICRO_VERSION 8
 
 #define RB_GRN_OBJECT(object) ((RbGrnObject *)(object))
 #define RB_GRN_NAMED_OBJECT(object) ((RbGrnNamedObject *)(object))
