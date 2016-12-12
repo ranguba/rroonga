4e183176de453681426a24f31e433eacb4637c67
Kouhei Sutou <kou@clear-code.com>
2016-12-12 23:23:53 +0900

    Use English for newly written document

--- ext/groonga/rb-grn-table.c
+++ ext/groonga/rb-grn-table.c
@@ -229,9 +229,9 @@ rb_grn_table_inspect (VALUE self)
  *   @option options :compress
  *     値の圧縮方法を指定する。省略した場合は、圧縮しない。
  *
- *     * `:zlib`: 値をzlib圧縮して格納する。
- *     * `:lz4`: 値をLZ4圧縮して格納する。
- *     * `:zstd`: 値をZstandard圧縮して格納する。
+ *     * `:zlib`: Compressed by zlib.
+ *     * `:lz4`: Compressed by LZ4.
+ *     * `:zstd`: Compressed by Zstandard.
  *
  * @return [Groonga::FixSizeColumn, Groonga::VariableSizeColumn]
  */
