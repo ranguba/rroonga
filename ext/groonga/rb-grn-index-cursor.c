3757d6e6687c69d87c35832e61168358b151c5e5
Yasuhiro Horimoto <horimoto@clear-code.com>
2019-06-28 23:48:54 +0900

    IndexCursor: use a "rb_define_singleton_method"

--- ext/groonga/rb-grn-index-cursor.c
+++ ext/groonga/rb-grn-index-cursor.c
@@ -175,8 +175,8 @@ rb_grn_init_index_cursor (VALUE mGrn)
     rb_define_method(rb_cGrnIndexCursor, "next", rb_grn_index_cursor_next, 0);
     rb_define_method(rb_cGrnIndexCursor, "each", rb_grn_index_cursor_each, -1);
 
-    rb_define_method(rb_cGrnIndexCursor, "set_min",
-                     rb_grn_index_cursor_s_set_min_p, 0);
-    rb_define_method(rb_cGrnIndexCursor, "set_min=",
-                     rb_grn_index_cursor_s_set_min_set, 1);
+    rb_define_singleton_method(rb_cGrnIndexCursor, "set_min",
+                               rb_grn_index_cursor_s_set_min_p, 0);
+    rb_define_singleton_method(rb_cGrnIndexCursor, "set_min=",
+                               rb_grn_index_cursor_s_set_min_set, 1);
 }
