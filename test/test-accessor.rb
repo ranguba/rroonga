e26c0eed68c1bc1ca9fcdf65c01c9833dca1b2f2
Sutou Kouhei <kou@clear-code.com>
2019-07-10 05:42:29 +0900

    Share common code

--- test/test-accessor.rb
+++ test/test-accessor.rb
@@ -21,10 +21,7 @@ class AccessorTest < Test::Unit::TestCase
     setup_database
     @posts = Groonga::Hash.create(:name => "Posts", :key_type => "ShortText")
     @id = @posts.column("_id")
-  end
-
-  def teardown
-    @id = nil
+    @key = @posts.column("_key")
   end
 
   def test_name
@@ -51,9 +48,8 @@ class AccessorTest < Test::Unit::TestCase
 
   sub_test_case "#key_accessor?" do
     test "true" do
-      key = @posts.column("_key")
       assert do
-        key.key_accessor?
+        @key.key_accessor?
       end
     end
 
@@ -72,9 +68,8 @@ class AccessorTest < Test::Unit::TestCase
     end
 
     test "false" do
-      key = @posts.column("_key")
       assert do
-        not key.id_accessor?
+        not @key.id_accessor?
       end
     end
   end
